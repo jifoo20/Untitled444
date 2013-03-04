b9bacc74a5badde0149f96c438ff2f3758a9a1ba
jifoo20 <jaafri.ing@gmail.com>
2013-03-04 14:29:00 +0000

    Update Untitled2.js

--- Untitled444/Untitled2.js
+++ Untitled444/Untitled2.js
@@ -1,4 +1,4 @@
-﻿var x="studio"
-//Use the "Try it yourself" examples on every page !!!
+Choosing the Follow menu command will force the selected file(s) to be monitored by Git. Each selected file is removed from the ".gitignore" file if it was listed in (for more information on this file, please refer to the [#title id="973069" anchor="987286"/] paragraph). If the selected file is not in the "Git ignore" file list, the command does nothing.
+When you select this command, the "followed" file will get the appropriate icon depending on its previous status (added, modified, removed). For more information, refer to the paragraph.
 
 //Change and experiment as much as possible with the code, and learn how everything works !!!
